c105ffe5abb5ff539143750f99c2a605ea0d0ce9
FromBenj <from@benj.com>
2026-08-19 17:37:17 +0200

    remove brush

--- src/js/lines.js
+++ src/js/lines.js
@@ -1,14 +1,40 @@
-export function drawLines(p, brush, palette, size) {
+export function drawLines(p, palette, size) {
     const width = size?.w;
     const height = size?.h;
     if (!p || !palette || !width || !height) return;
 
-    for (let i = 0; i < 80; i++) {
-        let x1 = 0;
-        let y1 = 0;
+    for (let i = 0; i < 60; i++) {
+        let x1 = width;
+        let y1 = height;
         let x2 = p.random(-0.5 * width, 0.5 * width);
         let y2 = p.random(-0.5 * height, 0.5 * height);
-        brush.set("explosion", p.random(palette), 1);
-        brush.line(x1, y1, x2, y2);
+        p.stroke(p.random(palette));
+
+        let dx = x2 - x1;
+        let dy = y2 - y1;
+        let len = p.sqrt(dx * dx + dy * dy);
+
+        for (let t = 0; t < 1; t += 0.02) {
+            let x = p.lerp(x1, x2, t);
+            let y = p.lerp(y1, y2, t);
+
+            let curve = p.map(
+                p.noise(i * 0.2, t * 8),
+                0, 1,
+                -15, 15
+            );
+
+            x += (-dy / len) * curve;
+            y += ( dx / len) * curve;
+
+            let nextT = t + 0.02;
+            let nextX = p.lerp(x1, x2, nextT);
+            let nextY = p.lerp(y1, y2, nextT);
+
+            let weight = p.lerp(5, p.random(15, 20), t);
+
+            p.strokeWeight(weight);
+            p.line(x, y, nextX, nextY);
+        }
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/explosion/',
+    build: {
+        chunkSizeWarningLimit: 500,
+    }
 })
