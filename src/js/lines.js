f494082454f5febb8c2b960932df7022da959636
FromBenj <from@benj.com>
2026-08-19 23:51:28 +0200

    lines animation

--- src/js/lines.js
+++ src/js/lines.js
@@ -1,25 +1,25 @@
-export function drawLines(p, palette, size) {
+export function drawLines(linesLayer, linesNbr, palette, size) {
     const width = size?.w;
     const height = size?.h;
-    if (!p || !palette || !width || !height) return;
+    if (!linesLayer || !palette || !width || !height) return;
 
-    for (let i = 0; i < 60; i++) {
+    for (let i = 0; i < linesNbr; i++) {
         let x1 = width / 2;
         let y1 = height / 2;
-        let x2 = p.random(-0.1, 1.1 * width);
-        let y2 = p.random(-0.2 * height / 2, 1.2 * height);
-        p.stroke(p.random(palette));
+        let x2 = linesLayer.random(-0.1, 1.1 * width);
+        let y2 = linesLayer.random(-0.2 * height / 2, 1.2 * height);
+        linesLayer.stroke(linesLayer.random(palette));
 
         let dx = x2 - x1;
         let dy = y2 - y1;
-        let len = p.sqrt(dx * dx + dy * dy);
+        let len = linesLayer.sqrt(dx * dx + dy * dy);
 
         for (let t = 0; t < 1; t += 0.02) {
-            let x = p.lerp(x1, x2, t);
-            let y = p.lerp(y1, y2, t);
+            let x = linesLayer.lerp(x1, x2, t);
+            let y = linesLayer.lerp(y1, y2, t);
 
-            let curve = p.map(
-                p.noise(i * 0.2, t * 8),
+            let curve = linesLayer.map(
+                linesLayer.noise(i * 0.2, t * 8),
                 0, 1,
                 -15, 15
             );
@@ -28,13 +28,13 @@ export function drawLines(p, palette, size) {
             y += (dx / len) * curve;
 
             let nextT = t + 0.02;
-            let nextX = p.lerp(x1, x2, nextT);
-            let nextY = p.lerp(y1, y2, nextT);
+            let nextX = linesLayer.lerp(x1, x2, nextT);
+            let nextY = linesLayer.lerp(y1, y2, nextT);
 
-            let weight = p.lerp(5, p.random(15, 20), t);
+            let weight = linesLayer.lerp(5, linesLayer.random(15, 20), t);
 
-            p.strokeWeight(weight);
-            p.line(x, y, nextX, nextY);
+            linesLayer.strokeWeight(weight);
+            linesLayer.line(x, y, nextX, nextY);
         }
     }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -6,36 +6,84 @@ import download from "./js/download";
 let canvas;
 
 new p5((p) => {
-    p.setup = async () => {
-        const wave = await p.loadImage(import.meta.env.BASE_URL + "images/waves.png");
-        const woman = await p.loadImage(import.meta.env.BASE_URL + "images/screaming-woman.png");
-        const papers = await p.loadImage(import.meta.env.BASE_URL + "images/papers-merged.png");
-        const signature = await p.loadImage(import.meta.env.BASE_URL + "images/signature.png");
 
-        const size = getSize(p);
-        const palette = params?.sunsetPalette?.map((c) => p.color(c));
-        if (!palette?.length) return;
+    let size, palette;
+    let papers, wave, woman, signature;
+    let linesLayer;
 
-
-        p.createCanvas(size.w, size.h);
+    const drawBackground = () => {
         p.background(p.color("#fb90bba8"));
         p.imageMode(p.CENTER);
         p.image(papers, size.w / 2, size.h / 2, size.w, size.h);
         p.image(wave, size.w / 2, size.h / 2, (size.max + size.min) / 2, (size.max + size.min) / 2);
+    };
 
-        p.strokeCap(p.ROUND);
-        drawLines(p, palette, size);
-
+    const drawWoman = () => {
         p.push();
         p.translate(size.w / 2, size.h / 2);
         p.rotate(p.PI);
         p.image(woman, 0, 0, size.min * 0.75, size.min * 0.75 * woman.height / woman.width);
         p.pop();
+    };
 
+    const drawSignature = () => {
         const signSize = 0.10 * size.min;
         const margin = signSize / 4;
         p.image(signature, size.w - signSize / 2 - margin, size.h - signSize / 2 - margin, signSize, signSize);
+    };
 
+    const refreshLines = (linesNbr) => {
+        linesLayer = p.createGraphics(size.w, size.h);
+        linesLayer.strokeCap(linesLayer.ROUND);
+        drawLines(linesLayer, linesNbr, palette, size);
+
+        drawBackground();
+
+        p.push();
+        p.imageMode(p.CORNER);
+        p.image(linesLayer, 0, 0);
+        p.pop();
+
+        drawWoman();
+        drawSignature();
+    };
+
+
+    p.setup = async () => {
+        size = getSize(p);
+        papers = await p.loadImage(import.meta.env.BASE_URL + "images/papers-merged.png");
+        wave = await p.loadImage(import.meta.env.BASE_URL + "images/waves.png");
+        woman = await p.loadImage(import.meta.env.BASE_URL + "images/screaming-woman.png");
+        signature = await p.loadImage(import.meta.env.BASE_URL + "images/signature.png");
+
+        p.createCanvas(size.w, size.h);
+        p.background(p.color("#fb90bba8"));
+
+        palette = params?.sunsetPalette?.map((c) => p.color(c));
+        if (!palette?.length) return;
+
+        p.imageMode(p.CENTER);
+        p.image(papers, size.w / 2, size.h / 2, size.w, size.h);
+        p.image(wave, size.w / 2, size.h / 2, (size.max + size.min) / 2, (size.max + size.min) / 2);
+
+        refreshLines(linesNbr);
         download(p, canvas, size);
+    };
+
+    let lastLinesAt = 0;
+    let rate = 600;
+    let linesNbr = 15;
+
+    p.draw = () => {
+        if (p.millis() - lastLinesAt > rate) {
+            lastLinesAt = p.millis();
+            if (linesLayer) {
+                linesLayer.remove();
+                refreshLines(linesNbr);
+                rate = rate * 0.9;
+                linesNbr += 5;
+                if (linesNbr >= 120) linesNbr = 120;
+            }
+        }
     };
 });
