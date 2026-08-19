3a66dbbf4ab5dfc1233140085b44f061600b18a1
FromBenj <from@benj.com>
2026-08-19 17:48:32 +0200

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/creative/embalada/',
+    base: '/explosion/',
     build: {
         chunkSizeWarningLimit: 1500,
     }
